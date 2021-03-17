d79ace76880725e027e3dcc1d5f8fba53df96758
Andrej Utz <andrej.utz@st.othr.de>
2021-03-17 16:34:06 +0100

    users: Add Utz as administrator

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,12 @@
 				shell = pkgs.fish;
 				openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCpcUE+pheherwiudaf8vEjJn0Ey9F+ZjwJtNEPpYPcK0a5om06EgcsVi39ggZCNeX7lyK09pqzJKgMTlfyQWWv4U5w0hBEBresgfOxHTAc+MzJ+dfHzUqeMNTKUFP7sZwDjFboshRmZRd7FX4bIcLmbe1TxWBz/XAr7g7lOD7hZJOFj/b8vlVbSZjOdZ23vIdf9RldY2ULdDK/EuHE5fFRbX6vVmidYjHl8CX7VPKkT3VhiXGhl2T7DEAScpRs8dOztY8sVeK5GLW78ozVbessODi3ErziejpygfRJ1JhslQa7Vwf/Lsd5RS6vSYafvLYCXT7scvbGE+gFmXtg9wR7Q8Z0i42JAUTDZdqpJ09Qp6slh/fWQ65Ym3lHcYHt/lqksUOBX+t8uF+o3ST6PGqoFxDeYY7kvyMgRjqvnsNyswUM179BbJrccgQPUbpHZO76l9+tYPRUF4sGdiVADbgMp6PXqt6wax+vlIb3CBj4gJCkZxf6jrK2LfqwL86NZIOe0RX1CegXJH6rYFTvHe7PzdbudupojmmpJWF/imD6ULm9W0tUsWJLpPzMnfnpbqmhMYRRRUBYP7Mf4tBCaxc2XK9VGQzZsmsa6Yf4KqOTOoz64fnceLye3O+CB9/JQ0t7HYOoSPI/X4KqNbEogAa5+uw/3/wIM1acH3YdQM1ufw==" ];
 			};
-
+			uta36888 = {
+				isNormalUser = true;
+				extraGroups = [ "wheel" ];
+				shell = pkgs.fish;
+				openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdsma74LYn5VdICUm1KgPte4rO563sWXJQfoVGGdfe6"];
+			};
 		};
 	};
 

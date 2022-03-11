cc81a213716c7d9ad69b8cce6f2917b371712563
Andrej Utz <andrej.utz@st.othr.de>
2022-03-11 03:23:33 +0100

    zulip: update to 4.10

--- configuration.nix
+++ configuration.nix
@@ -255,7 +255,7 @@ in
 
   virtualisation.oci-containers.containers = rec {
     chat = {
-      image = "zulip/docker-zulip:4.8-1";
+      image = "zulip/docker-zulip:4.10-0";
       dependsOn = [ "chat-db" "chat-cache" "chat-mqueue" ];
       # hack
       cmd = [ "/bin/sh" "-c" "/home/zulip/deployments/current/scripts/zulip-puppet-apply -f && entrypoint.sh app:run" ];

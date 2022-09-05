5876192ce042a45126da29af07aa60a8131e79d4
Andrej Utz <andrej.utz@st.othr.de>
2022-09-05 04:45:13 +0200

    zulip: update to 5.6

--- configuration.nix
+++ configuration.nix
@@ -311,7 +311,7 @@ in
 
   virtualisation.oci-containers.containers = rec {
     chat = {
-      image = "zulip/docker-zulip:5.2-0";
+      image = "zulip/docker-zulip:5.6-0";
       dependsOn = [ "chat-db" "chat-cache" "chat-mqueue" ];
       # hack
       cmd = [ "/bin/sh" "-c" "/home/zulip/deployments/current/scripts/zulip-puppet-apply -f && entrypoint.sh app:run" ];

870f1fe032d2818bc209768e621f3df970fd638f
Andrej Utz <andrej.utz@st.othr.de>
2023-10-27 14:47:21 +0200

    postgresql: tune config to our hardware to improve performance

--- configuration.nix
+++ configuration.nix
@@ -255,6 +255,26 @@ in
           ensurePermissions."DATABASE ${config.services.hedgedoc.settings.db.database}" = "ALL PRIVILEGES";
         }
       ];
+      settings = {
+        log_min_duration_statement = 1000;
+        log_min_messages = "LOG";
+        #log_statement = "mod";
+        log_destination = lib.mkForce "syslog";
+        #log_connections = true;
+      
+        # Based on https://www.pgconfig.org/#/?max_connections=256&pg_version=14&environment_name=WEB&total_ram=64&cpus=8&drive_type=HDD&arch=x86-64&os_type=linux
+        # Memory
+        shared_buffers = "16GB";
+        effective_cache_size = "48GB";
+        work_mem = "64MB";
+        maintenance_work_mem = "4GB";
+        # Checkpoint related
+        min_wal_size = "2GB";
+        max_wal_size = "3GB";
+        # Storage
+        random_page_cost = 5;
+        effective_io_concurrency = 2;
+      };
     };
     # Database for Examia
     mysql = {

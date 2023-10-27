00b7001921a0444f1c024f0c325a8bfa99dcba5c
Andrej Utz <andrej.utz@st.othr.de>
2023-10-27 19:10:50 +0200

    Add new service: Minecraft
    OTH-VPN may be required for access.

--- configuration.nix
+++ configuration.nix
@@ -486,6 +486,38 @@ in
         "/var/lib/zulip/rabbitmq:/var/lib/rabbitmq:rw"
       ];
       extraOptions = [ "--network=container:chat-db" ];
+    }; 
+
+    minecraft = {
+      image = "itzg/minecraft-server:latest";
+      environment = {
+        MOTD = "FSIM Anarchie";
+        VIEW_DISTANCE = "16";
+        DIFFICULTY = "hard";
+
+        SEED = "1785852800490497919";
+        SPAWN_PROTECTION = "15";
+        PLAYER_IDLE_TIMEOUT = "30";
+
+        MEMORY = "16G";
+        INIT_MEMORY = "8G";
+        MAX_MEMORY = "64G";
+        MAX_PLAYERS = "128";
+        USE_AIKAR_FLAGS = "TRUE";
+
+        RCON_PASSWORD = "8642753";
+
+        TYPE = "PAPER";
+        EULA = "TRUE";
+        EXISTING_WHITELIST_FILE = "MERGE";
+        USE_SIMD_FLAGS = "TRUE";
+        UID = "25565";
+        GID = "25565";
+      };
+      volumes = [
+        "/var/lib/minecraft:/data:rw"
+      ];
+      ports = [ "25565:25565" ];
     };
   };
 
